4d628731a830427f71a5428e6693e0a4738dce08
Egor Vasyakin <e.vasyakin@itevas.ru>
2024-11-11 06:15:16 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Библиотека с сахаром для упрощения других библиотек.
+ * @package @evas/vue-help
+ * @author Egor Vasyakin <egor@evas-php.com>
+ * @license CC-BY-4.0
+ */
+
+import './define.js'
+export { getEnv } from './getEnv.js'
